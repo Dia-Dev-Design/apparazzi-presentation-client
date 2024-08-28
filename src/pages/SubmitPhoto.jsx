4fc4d1ba82a2c342b64b372d5673196d4523dc81
Test <Hammertyme101@gmail.com>
2024-08-28 16:39:36 -0400

    addedstyledcomponents-addedthemtosubmitphotopage-addedimagepreview-updatedmongoose

--- src/pages/SubmitPhoto.jsx
+++ src/pages/SubmitPhoto.jsx
@@ -1,10 +1,62 @@
 import { useState } from "react";
+import styled from 'styled-components';
 import { post } from "../services/authService";
 import { useNavigate } from "react-router-dom";
 import { newPhoto } from "../services/fileChange";
 import { convertGPS } from "../services/convertGPS";
-
 import { returnMapTime } from "../services/time";
+
+const SubmitPhotoContainer = styled.div`
+  max-width: 800px;
+  margin: 40px auto;
+  padding: 15px;
+  background-color: white;
+  border: 1px solid lightgrey;
+  border-radius: 10px;
+  display: flex;
+`;
+
+const Form = styled.form`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Label = styled.label`
+  margin-bottom: 5px;
+`;
+
+const Input = styled.input`
+  padding: 5px;
+  margin-bottom: 20px;
+  border: 1px solid black;
+  border-radius: 5px;
+`;
+
+const Button = styled.button`
+  padding: 10px 20px;
+  background-color: black;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const ErrorMessage = styled.p`
+  color: red;
+  margin-top: 10px;
+`;
+
+const ImagePreviewContainer = styled.div`
+  margin-left: 20px;
+  width: 200px;
+`;
+
+const ImagePreview = styled.img`
+  max-width: 100%;
+  height: auto;
+  border-radius: 10px;
+`;
 
 const SubmitPhoto = () => {
   const [photo, setPhoto] = useState({
@@ -16,7 +68,9 @@ const SubmitPhoto = () => {
 
   const [disabled, setDisabled] = useState(false);
 
-  const [errorMessage, setErrorMessage] = useState('')
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const [imagePreview, setImagePreview] = useState(null);
 
   let navigate = useNavigate();
 
@@ -83,6 +137,13 @@ const SubmitPhoto = () => {
       .finally(() => {
         setDisabled((prev) => !prev);
       });
+
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      setImagePreview(e.target.result);
+    };
+    reader.readAsDataURL(file);
   };
 
   const handleSubmit = (e) => {
@@ -99,61 +160,63 @@ const SubmitPhoto = () => {
       latitude: photo.latitude,
       longitude: photo.longitude,
       photographedDate: photo.photographedDate,
+    })      .then(() => {
+      navigate("/profile");
     })
-      .then(() => {
-        navigate("/profile");
-      })
-      .catch((error) => {
-        console.error("There was an error!", error);
-      });
-  };
+    .catch((error) => {
+      console.error("There was an error!", error);
+    });
+};
 
-  return (
-    <div className="homeLanding">
-      <div className="homeContainer">
-        <form onSubmit={handleSubmit}>
-          <label>
-            New Photo
-            <input onChange={handleFileUpload} type="file" name="imageUrl" />
-          </label>
+return (
+  <SubmitPhotoContainer>
+    <Form onSubmit={handleSubmit}>
+      <Label>
+        New Photo
+        <Input onChange={handleFileUpload} type="file" name="imageUrl" />
+      </Label>
 
-          <br />
+      <br />
 
-          <label>
-            Description
-            <input
-              onChange={handleTextChange}
-              type="text"
-              name="description"
-              value={photo.description}
-            />
-          </label>
+      <Label>
+        Description
+        <Input
+          onChange={handleTextChange}
+          type="text"
+          name="description"
+          value={photo.description}
+        />
+      </Label>
 
-          <br />
+      <br />
 
-          <label>
-            Tags
-            <input
-              onChange={handleTextChange}
-              type="text"
-              name="tags"
-              value={photo.tags}
-            />
-          </label>
+      <Label>
+        Tags
+        <Input
+          onChange={handleTextChange}
+          type="text"
+          name="tags"
+          value={photo.tags}
+        />
+      </Label>
 
-          <p>
-            When submitting photo tags, please seperate them by a comma and a
-            space. Like so: "John Doe, Rocket Launch". Thank you.
-          </p>
+      <p>
+        When submitting photo tags, please seperate them by a comma and a
+        space. Like so: "John Doe, Rocket Launch". Thank you.
+      </p>
 
-          <button disabled={disabled} type="sumbit">
-            Submit Photo
-          </button>
-        </form>
-          {errorMessage && <p>{errorMessage}</p>}
-      </div>
-    </div>
-  );
+      <Button disabled={disabled} type="submit">
+        Submit Photo
+      </Button>
+      {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
+    </Form>
+    {imagePreview && (
+      <ImagePreviewContainer>
+        <ImagePreview src={imagePreview} alt="Image Preview" />
+      </ImagePreviewContainer>
+    )}
+  </SubmitPhotoContainer>
+);
 };
 
 export default SubmitPhoto;
